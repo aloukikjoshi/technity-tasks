5b86be262e2e4780264a7c23b81137d09c979bb3
Aloukik Joshi <119406434+aloukikjoshi@users.noreply.github.com>
2023-09-13 16:03:00 +0530

    Create 2.py

---
+++ task-5/Chapter 11/2.py
@@ -0,0 +1,6 @@
+eggs = "goodbye"
+bacon = "GOODbye"
+
+assert eggs.lower() != "goodbye".lower(), "eggs and bacon should not have the same value as 'Goodbye', regardless of case"
+
+print("The values of 'eggs' and 'bacon' are different from 'Goodbye', regardless of case.")
